c6ccdbf1ba0340bca5889b65cec4ee8d67f0bd67
Brian Liu <b_liu72@yahoo.com>
2022-12-03 00:23:55 -0500

    Add cat facts

---
+++ src/components/CatFacts.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  data() {
+    return {
+      fact: '',
+      loading: false,
+    }
+  },
+  methods: {
+    fetchFact() {
+      this.loading = true;
+      fetch('https://meowfacts.herokuapp.com/')
+        .then(response => response.json())
+        .then((data) => {
+          this.fact = data.data[0];
+          this.loading = false;
+        })
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h3>{{ fact }}</h3>
+    <button @:click="fetchFact">
+      Get a cat fact
+    </button>
+  </div>
+</template>
